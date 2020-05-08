17444b8f7b4bec2362432c3b1873560a7e891d60
Tom Clayton <tgclayton1@gmail.com>
2020-05-09 11:56:51 +1200

    update

--- client/components/game.js
+++ client/components/game.js
@@ -21,79 +21,85 @@ export default {
   }
 }
 
-// var game = new Phaser.Game(config)
+const teamSize = 5
+var team1 = 'team1'
+var team2 = 'team2'
+
+var actors = [{ name: team1 }, { name: team2 }]
 var map = null
 map = createMapArray()
 console.log(map)
 
-var actors = [
-  {
-    team: 1,
-    units: [
-      {
-        name: 'warrior2H',
-        sprite: 'warrior',
-        idx: 160
+function createActors () {
+  for (let i = 0; i < teamSize; i++) {
 
-      },
-      {
-        name: 'warrior2',
-        sprite: 'warrior',
-        idx: 180
-      },
-      {
-        name: 'warrior3',
-        sprite: 'warrior',
-        idx: 200
-      }
-    ]
-  },
-  {
-    team: 2,
-    units: [
-      {
-        name: 'enemy1',
-        sprite: 'enemywarrior',
-        idx: 179
-      },
-      {
-        name: 'enemy2',
-        sprite: 'enemywarrior',
-        idx: 199
-      },
-      {
-        name: 'enemy3',
-        sprite: 'enemywarrior',
-        idx: 219
-      }
-    ]
   }
-]
+}
 
-// var game = new Phaser.Game(config)
+// var actors = [
+//   {
+//     team: 1,
+//     units: [
+//       {
+//         name: 'warrior2H',
+//         sprite: 'warrior',
+//         idx: 160
+
+//       },
+//       {
+//         name: 'warrior2',
+//         sprite: 'warrior',
+//         idx: 180
+//       },
+//       {
+//         name: 'warrior3',
+//         sprite: 'warrior',
+//         idx: 200
+//       }
+//     ]
+//   },
+//   {
+//     team: 2,
+//     units: [
+//       {
+//         name: 'enemy1',
+//         sprite: 'enemywarrior',
+//         idx: 179
+//       },
+//       {
+//         name: 'enemy2',
+//         sprite: 'enemywarrior',
+//         idx: 199
+//       },
+//       {
+//         name: 'enemy3',
+//         sprite: 'enemywarrior',
+//         idx: 219
+//       }
+//     ]
+//   }
+// ]
+
+var game = new Phaser.Game(config)
 var player
 var cursor
-var cursor
 var cursors
-var keyZ
-var keyX
 
 function preload () {
-  this.load.image('warrior', '/assets/images/warrior.png')
-  this.load.image('enemywarrior', '/assets/images/enemywarrior.png')
-  this.load.image('gcursor', '/assets/images/green-cursor.png')
-  this.load.image('bcursor', '/assets/images/blue-cursor.png')
-  this.load.image('rcursor', '/assets/images/red-cursor.png')
-  this.load.image('testmap2', '/assets/images/testmap2.png')
+  this.load.image('warrior', 'src/assets/images/warrior.png')
+  this.load.image('enemywarrior', 'src/assets/images/enemywarrior.png')
+  this.load.image('gcursor', 'src/assets/images/green-cursor.png')
+  this.load.image('bcursor', 'src/assets/images/blue-cursor.png')
+  this.load.image('rcursor', 'src/assets/images/red-cursor.png')
+  this.load.image('testmap2', 'src/assets/images/testmap2.png')
 }
 
 function create () {
-  this.input.keyboard.on('keydown-Z', checkTile, this)
-  this.input.keyboard.on('keydown-X', changeCursorColor, this)
+  this.input.keyboard.on('keydown', keyDown, this)
+  // this.input.keyboard.on('keydown-Z', checkTile, this)
+  // this.input.keyboard.on('keydown-X', changeCursorColor, this)
   this.add.image(480, 480, 'testmap2')
   cursors = this.input.keyboard.createCursorKeys()
-  keyZ = this.input.keyboard.addKey('Z')
-  keyX = this.input.keyboard.addKey('X')
 
   actors.forEach(team => {
     team.units.forEach(actor => {
@@ -109,6 +115,20 @@ function create () {
   player.setData('notMoving', true)
   player.setData('idx', 0)
   player.setData('sprite', 'gcursor')
+}
+
+function update () {
+  if (player.getData('notMoving')) {
+    if (cursors.left.isDown) {
+      setfixedMovement(-48, 'x')
+    } else if (cursors.right.isDown) {
+      setfixedMovement(48, 'x')
+    } else if (cursors.up.isDown) {
+      setfixedMovement(-48, 'y')
+    } else if (cursors.down.isDown) {
+      setfixedMovement(48, 'y')
+    }
+  }
 }
 
 function getCoordsFromIndex (idx) {
@@ -133,22 +153,20 @@ function setfixedMovement (val, axis) {
 }
 
 function setCursorIndex () {
-  // console.log('x: ', player.x, 'y:', player.y)
-  player.setData('idx', ((player.x / 48) + ((player.y / 48) * 20)))
-}
-
-function update () {
-  if (player.getData('notMoving')) {
-    if (cursors.left.isDown) {
-      setfixedMovement(-48, 'x')
-    } else if (cursors.right.isDown) {
-      setfixedMovement(48, 'x')
-    } else if (cursors.up.isDown) {
-      setfixedMovement(-48, 'y')
-    } else if (cursors.down.isDown) {
-      setfixedMovement(48, 'y')
-    }
+  let x = player.x / 48
+  let y = (player.y / 48) * 20
+  if (x > 19) {
+    x = 19
+  } else if (x < 0) {
+    x = 0
   }
+  if (y > 380) {
+    y = 380
+  } else if (y < 0) {
+    y = 0
+  }
+  // console.log('x: ', player.x, 'y:', player.y)
+  player.setData('idx', x + y)
 }
 
 function checkTile () {
@@ -171,7 +189,7 @@ function checkTile () {
   }
 }
 
-function changeCursorColor () {
+function changeCursorColor (context) {
   let sprite
   if (player.getData('sprite') === 'gcursor') {
     sprite = 'bcursor'
@@ -181,11 +199,19 @@ function changeCursorColor () {
   let idx = player.getData('idx')
   let coords = getCoordsFromIndex(idx)
   player.destroy()
-  player = this.physics.add.image(coords[0], coords[1], sprite).setOrigin(0, 0)
+  player = context.physics.add.image(coords[0], coords[1], sprite).setOrigin(0, 0)
   player.setCollideWorldBounds(true)
   player.setData('notMoving', true)
   player.setData('idx', idx)
   player.setData('sprite', sprite)
   setCursorIndex()
   // cursor = this.physics.add.image(96, 96, 'bcursor').setOrigin(0, 0)
+}
+
+function keyDown (e) {
+  let key = e.key
+  switch (key) {
+    case 'x':
+      changeCursorColor(this)
+  }
 }
