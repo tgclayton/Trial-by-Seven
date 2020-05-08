ba13482f0aa96d45af4f6fcecf83425867feb6a6
Karel Fourie <karelmakazi@gmail.com>
2020-05-08 15:20:42 +1200

    intial branch commit

---
+++ src/components/phaserSource.js
@@ -0,0 +1,103 @@
+import Phaser from 'phaser'
+
+var actors = [
+  {
+    name: 'warrior1',
+    x: 48,
+    y: 48
+  },
+  {
+    name: 'warrior2',
+    x: 48,
+    y: 92
+  },
+  {
+    name: 'warrior3',
+    x: 48,
+    y: 144
+  },
+  {
+    name: 'enemy1',
+    x: 48,
+    y: 192
+  },
+  {
+    name: 'enemy2',
+    x: 48,
+    y: 240
+  },
+  {
+    name: 'enemy3',
+    x: 48,
+    y: 288
+  }
+
+]
+var config = {
+  type: Phaser.AUTO,
+  width: 960,
+  height: 960,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+}
+
+var game = new Phaser.Game(config)
+var player
+var cursor
+var cursors
+
+function preload () {
+  this.load.image('warrior', 'src/assets/images/warrior.png')
+  this.load.image('cursor', 'src/assets/images/green-cursor.png')
+  this.load.image('testmap2', 'src/assets/images/testmap2.png')
+}
+
+function create () {
+  this.add.image(480, 480, 'testmap2')
+  cursors = this.input.keyboard.createCursorKeys()
+
+  actors.forEach((actor, idx) => {
+    actors[idx] = this.physics.add.image(actor.x, actor.y, 'warrior').setOrigin(0, 0)
+  })
+  player = this.physics.add.image(0, 0, 'cursor').setOrigin(0, 0)
+  player.setCollideWorldBounds(true)
+  player.setData('notMoving', true)
+}
+
+function setfixedMovement (val, axis) {
+  if (axis === 'x') {
+    player.x += val
+  } else if (axis === 'y') {
+    player.y += val
+  }
+
+  player.setPosition(player.x, player.y)
+  player.setData('notMoving', false)
+  setTimeout(() => {
+    player.setData('notMoving', true)
+  }, 100)
+}
+
+function update () {
+  if (player.getData('notMoving')) {
+    if (cursors.left.isDown) {
+      setfixedMovement(-48, 'x')
+    } else if (cursors.right.isDown) {
+      setfixedMovement(48, 'x')
+    } else if (cursors.up.isDown) {
+      setfixedMovement(-48, 'y')
+    } else if (cursors.down.isDown) {
+      setfixedMovement(48, 'y')
+    }
+  }
+}
