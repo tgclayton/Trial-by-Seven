07410569a3e79f3acb9569606653e59eb92d3d89
Stan-Gogan <sjgogan@me.com>
2020-05-10 11:19:44 +1200

    sign up form

--- client/components/TitleForm.jsx
+++ client/components/TitleForm.jsx
@@ -26,23 +26,26 @@ class TitleForm extends React.Component {
       <div className="formBody">
         <Form>
             <h3 className="formTitleText">What should the heralds call you?</h3>
+            <div className="formFlex">
+              <h2>Champion one</h2>
           <Form.Group className="formGroup">
-            <div>
             <Form.Input
-              label='championOne'
               placeholder='championOne'
               name='championOne'
               onChange={event => this.setState({championOne: event.target.value })}
-             /></div>
+             />
           </Form.Group>
+          </div>
+            <div className="formFlex">
+              <h2>Champion Two</h2>
           <Form.Group>
             <Form.Input
-              label='championTwo'
               placeholder='championTwo'
               name='championTwo'
               onChange={event => this.setState({championTwo: event.target.value })}
-             />
+                />
           </Form.Group>
+            </div>
           <Form.Button onClick={this.submitHandler}>Submit</Form.Button>
           <Link to='/game'>Temporary Link to PhaserGame</Link>
         </Form>
