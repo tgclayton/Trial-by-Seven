45e09db068d3922c6526b0df762ff9ee9d806204
Tom Clayton <tgclayton1@gmail.com>
2020-05-13 07:51:34 +1200

    endgame button appears in end game window

--- client/components/EndGame.jsx
+++ client/components/EndGame.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class EndGame extends React.Component {
   constructor (props) {
@@ -12,6 +13,9 @@ class EndGame extends React.Component {
     return (
       <div id = 'winBox'className='endgame-container hidden'>
         <h1 id = 'endRes'>{this.state.winner} wins</h1>
+        <div className="endGameDiv">
+          <Link to='/report'><div className="endGameButton"><h1 className="endGameText">End Game</h1></div></Link>
+        </div>
       </div>
     )
   }
--- client/components/PhaserGame.jsx
+++ client/components/PhaserGame.jsx
@@ -56,9 +56,6 @@ class PhaserGame extends Component {
             </div>
             <ChampionTwo />
           </div>
-          <div className="endGameDiv">
-            <Link to='/report'><div className="endGameButton"><h1 className="endGameText">End Game</h1></div></Link>
-          </div>
         </div>
       </div>
     )
